636f35f705eb13b212d7a6f19383bfbc069fc92c
ServicityUSA <servicityusa@gmail.com>
2025-01-05 12:34:52 -0500

    Adds prisma and db connection.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,14 +1,12 @@
-import sampleData from "@/db/sample-data";
+import { getLatestProducts } from "@/lib/actions/product.actions";
 import ProductList from "@/components/shared/product/product-list";
 
-export default function HomePage() {
+export default async function HomePage() {
+  const latestProducts = await getLatestProducts();
+
   return (
     <>
-      <ProductList
-        data={sampleData.products}
-        title="Newest Arrivals"
-        limit={4}
-      />
+      <ProductList data={latestProducts} title="Newest Arrivals" limit={4} />
     </>
   );
 }
